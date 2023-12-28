5d85aca3d2896e0e23ed1da5ba461638a98b204f
Teratto <crunchapotato@gmail.com>
2023-12-27 21:07:38 -0800

    To do: add more cards and figure out how symbols work lol

---
+++ DemoMod/Cards/DONOTUSETESTCODE.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace DemoMod.Cards
+//{
+//    internal class DONOTUSETESTCODE
+//    {
+//    }
+//}
+
+/*
+
+list.Add(new AStatus()
+{
+    status = TaxationStatusPatches.TaxationStatus,
+    statusAmount = 1
+});
+
+*/
+
