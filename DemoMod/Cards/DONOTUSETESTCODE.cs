834a5c81a085011951c5244461ef27a8ccfa40dd
Teratto <Crunchapotato@gmail.com>
2023-12-30 00:03:20 -0800

    cards

--- DemoMod/Cards/DONOTUSETESTCODE.cs
+++ DemoMod/Cards/DONOTUSETESTCODE.cs
@@ -21,8 +21,11 @@ list.Add(new AStatus()
 
 */
 
-//TO DO FOR STATUSES
-//1. Make a "missingTera" status
+//TO DO LIST
 //2. Make a "engine stall" next turn
 //3. Make an "engine lock" next turn
 //4. Make a "Tax enemy once every turn" status
+//5. Make Ataxing attack not red, and also have it display its damage and tax application separately
+//6. How do conditional cards???
+//7. How do cards related to energy?
+//8. Potential drone that applies tax?
