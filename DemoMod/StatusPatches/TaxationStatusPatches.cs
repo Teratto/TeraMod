6049950ffc6aea9c0781a0a355ab594d4c160581
ceresgalax <ceresgalax@gmail.com>
2024-01-01 20:52:27 -0800

    Remove ATaxingAttack, as AAtack has a status field.

--- DemoMod/StatusPatches/TaxationStatusPatches.cs
+++ DemoMod/StatusPatches/TaxationStatusPatches.cs
@@ -21,10 +21,10 @@ namespace DemoMod.StatusPatches
                 postfix: new HarmonyMethod(typeof(TaxationStatusPatches), nameof(Ship_OnAfterTurn_Postfix))
             );
 
-            harmony.Patch(
-                original: AccessTools.DeclaredMethod(typeof(AAttack), nameof(AAttack.Begin)),
-                transpiler: new HarmonyMethod(typeof(TaxationStatusPatches), nameof(AAttack_Begin_Transpiler))
-            );
+            //harmony.Patch(
+            //    original: AccessTools.DeclaredMethod(typeof(AAttack), nameof(AAttack.Begin)),
+            //    transpiler: new HarmonyMethod(typeof(TaxationStatusPatches), nameof(AAttack_Begin_Transpiler))
+            //);
         }
 
         private static void Ship_OnAfterTurn_Postfix(Ship __instance, State s, Combat c)
@@ -41,50 +41,50 @@ namespace DemoMod.StatusPatches
             }
         }
 
-        private static IEnumerable<CodeInstruction> AAttack_Begin_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator, MethodBase original)
-        {
-            MethodInfo targetMethod = AccessTools.DeclaredMethod(typeof(Ship), nameof(Ship.NormalDamage));
-            MethodInfo methodToInject = AccessTools.DeclaredMethod(typeof(TaxationStatusPatches), nameof(InterceptAttackDamage));
+        //private static IEnumerable<CodeInstruction> AAttack_Begin_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator, MethodBase original)
+        //{
+        //    MethodInfo targetMethod = AccessTools.DeclaredMethod(typeof(Ship), nameof(Ship.NormalDamage));
+        //    MethodInfo methodToInject = AccessTools.DeclaredMethod(typeof(TaxationStatusPatches), nameof(InterceptAttackDamage));
 
-            bool wasAbleToInject = false;
+        //    bool wasAbleToInject = false;
 
-            foreach (CodeInstruction ins in instructions)
-            {
-                yield return ins;
+        //    foreach (CodeInstruction ins in instructions)
+        //    {
+        //        yield return ins;
 
-                if (ins.opcode == OpCodes.Callvirt)
-                {
-                    // It's a CallVirt instruction, is it called Ship::NormalDamage?
-                    MethodInfo calledMethod = (MethodInfo)ins.operand;
-                    if (calledMethod == targetMethod)
-                    {
-                        // Emit a call
-                        yield return new CodeInstruction(OpCodes.Ldarg_0); // this
-                        yield return new CodeInstruction(OpCodes.Ldarg_2); // s
-                        yield return new CodeInstruction(OpCodes.Ldarg_3); // c
-                        yield return new CodeInstruction(OpCodes.Call, methodToInject);
-                        // I'm also assuming that calling a void return type method will not push any result on the the execution stack... Fingers crossed!!
+        //        if (ins.opcode == OpCodes.Callvirt)
+        //        {
+        //            // It's a CallVirt instruction, is it called Ship::NormalDamage?
+        //            MethodInfo calledMethod = (MethodInfo)ins.operand;
+        //            if (calledMethod == targetMethod)
+        //            {
+        //                // Emit a call
+        //                yield return new CodeInstruction(OpCodes.Ldarg_0); // this
+        //                yield return new CodeInstruction(OpCodes.Ldarg_2); // s
+        //                yield return new CodeInstruction(OpCodes.Ldarg_3); // c
+        //                yield return new CodeInstruction(OpCodes.Call, methodToInject);
+        //                // I'm also assuming that calling a void return type method will not push any result on the the execution stack... Fingers crossed!!
 
-                        wasAbleToInject = true;
-                    }
-                }
-            }
+        //                wasAbleToInject = true;
+        //            }
+        //        }
+        //    }
 
-            if (!wasAbleToInject)
-            {
-                throw new Exception("AAA WASNT ABLE TO INJECT!!!");
-            }
-        }
+        //    if (!wasAbleToInject)
+        //    {
+        //        throw new Exception("AAA WASNT ABLE TO INJECT!!!");
+        //    }
+        //}
 
-        private static void InterceptAttackDamage(AAttack attackAction, State state, Combat combat)
-        {
-            Ship targetShip = attackAction.targetPlayer ? state.ship : combat.otherShip;
+        //private static void InterceptAttackDamage(AAttack attackAction, State state, Combat combat)
+        //{
+        //    Ship targetShip = attackAction.targetPlayer ? state.ship : combat.otherShip;
 
-            if (attackAction is ATaxingAttack taxingAttackAction)
-            {
-                targetShip.Add(TeraModStatuses.Taxation, taxingAttackAction.Tax);
-            }
-        }
+        //    if (attackAction is ATaxingAttack taxingAttackAction)
+        //    {
+        //        targetShip.Add(TeraModStatuses.Taxation, taxingAttackAction.Tax);
+        //    }
+        //}
 
     }
 }
