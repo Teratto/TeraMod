b5dd89d7155e9be2441d1c6f712a7da053f6e38c
ceresgalax <ceresgalax@gmail.com>
2024-02-06 23:16:23 -0800

    Fix Inflation artifact not behaving correctly.

--- Tera/StatusPatches/TaxationStatusPatches.cs
+++ Tera/StatusPatches/TaxationStatusPatches.cs
@@ -30,7 +30,7 @@ namespace Tera.StatusPatches
 
         private static void Ship_OnAfterTurn_Postfix(Ship __instance, State s, Combat c)
         {
-            bool inflationGet = s.artifacts.Find(a => a.GetType() == typeof(TeraArtifactInflation)) != null;  
+            bool inflationGet = s.EnumerateAllArtifacts().Find(a => a.GetType() == typeof(TeraArtifactInflation)) != null;  
             int taxationStatusAmmount = __instance.Get(TeraModStatuses.Taxation);
             int bigTaxTime = 3;
            
@@ -52,51 +52,6 @@ namespace Tera.StatusPatches
                 });
             }
         }
-
-        //private static IEnumerable<CodeInstruction> AAttack_Begin_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator, MethodBase original)
-        //{
-        //    MethodInfo targetMethod = AccessTools.DeclaredMethod(typeof(Ship), nameof(Ship.NormalDamage));
-        //    MethodInfo methodToInject = AccessTools.DeclaredMethod(typeof(TaxationStatusPatches), nameof(InterceptAttackDamage));
-
-        //    bool wasAbleToInject = false;
-
-        //    foreach (CodeInstruction ins in instructions)
-        //    {
-        //        yield return ins;
-
-        //        if (ins.opcode == OpCodes.Callvirt)
-        //        {
-        //            // It's a CallVirt instruction, is it called Ship::NormalDamage?
-        //            MethodInfo calledMethod = (MethodInfo)ins.operand;
-        //            if (calledMethod == targetMethod)
-        //            {
-        //                // Emit a call
-        //                yield return new CodeInstruction(OpCodes.Ldarg_0); // this
-        //                yield return new CodeInstruction(OpCodes.Ldarg_2); // s
-        //                yield return new CodeInstruction(OpCodes.Ldarg_3); // c
-        //                yield return new CodeInstruction(OpCodes.Call, methodToInject);
-        //                // I'm also assuming that calling a void return type method will not push any result on the the execution stack... Fingers crossed!!
-
-        //                wasAbleToInject = true;
-        //            }
-        //        }
-        //    }
-
-        //    if (!wasAbleToInject)
-        //    {
-        //        throw new Exception("AAA WASNT ABLE TO INJECT!!!");
-        //    }
-        //}
-
-        //private static void InterceptAttackDamage(AAttack attackAction, State state, Combat combat)
-        //{
-        //    Ship targetShip = attackAction.targetPlayer ? state.ship : combat.otherShip;
-
-        //    if (attackAction is ATaxingAttack taxingAttackAction)
-        //    {
-        //        targetShip.Add(TeraModStatuses.Taxation, taxingAttackAction.Tax);
-        //    }
-        //}
 
     }
 }
